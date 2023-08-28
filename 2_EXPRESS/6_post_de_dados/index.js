4f1dfb76c5528a7ec22e34cc012213871e64b366
EA - Ervin Lima Camargo <ervin_lima@educadventista.org>
2023-08-28 19:50:13 -0300

    desenvolvimento

--- 2_EXPRESS/6_post_de_dados/index.js
+++ 2_EXPRESS/6_post_de_dados/index.js
@@ -4,20 +4,23 @@ const app = express()
 const path = require('path')
 const caminho = path.join(__dirname, 'templates')
 
-app.get('/users/cadastrar',(req, res)=>{
+app.get('/users/cadastrar', (req, res) => {
     res.sendFile(`${caminho}/usuariosform.html`)
 })
+
+app.post('/users/save', (req, res) => {
+    console.log(req.body)
+}) 
 
 app.get('/users/:id', (req, res) => {
     const id = req.params.id
 
-    console.log(`estamos buscando pelo usuário: ${id}`)
+    console.log(`Estamos buscando pelo usuário: ${id}`)
     res.sendFile(`${caminho}/usuarios.html`)
 })
 
 app.get('/', (req, res) => {
     res.sendFile(`${caminho}/index.html`)
 })
-
 
 app.listen(3000)
